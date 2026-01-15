b16a445a5d76a4f2a4fb2cc23cb103b14a3340ee
CHARITHA SRI NAGALLA <charithas0014@gmail.com>
2026-01-15 15:18:10 +0530

    Add files via upload

---
+++ hello_world.py
@@ -0,0 +1,9 @@
+# Input Details From User
+name = input("Enter Name: ")
+internship_role = input("Enter Internship Role: ")
+# Fixed Date
+today_date = "15-01-2026"
+# Output Details
+print("Name: ",name)
+print("Internship Role: ",internship_role)
+print("Date: ",today_date)
